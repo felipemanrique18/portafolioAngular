1c2df3f8e36665223df15d05d4a2c744036b5452
felipe manrique <felipemanrique018@gmail.com>
2021-04-15 19:24:48 -0500

    Agregando funcionalidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { NofoundComponent } from './components/nofound/nofound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'**',pathMatch:'full',component:NofoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BannerComponent } from './components/shared/banner/banner.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NofoundComponent } from './components/nofound/nofound.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoadingComponent,
+    SobremiComponent,
+    PortafolioComponent,
+    FooterComponent,
+    BannerComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    NofoundComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
